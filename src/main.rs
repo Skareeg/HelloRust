61b05f2513ecbd16262e3c025ccb4467d32f99aa
Skareeg <skareeg@gmail.com>
2020-03-12 13:16:59 -0400

    They live!

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,40 @@ struct Renderable {
     bg: RGB
 }
 
+#[derive(Component)]
+struct LeftMover {}
+
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
+
 struct State {
     ecs: World
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
         ctx.print(1, 1, "Hello Rust World!");
+
+        self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -48,6 +74,7 @@ fn main() {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
 
     gs.ecs
         .create_entity()
@@ -60,6 +87,7 @@ fn main() {
             .create_entity()
             .with(Position { x: i * 7, y: 20 })
             .with(Renderable { glyph: rltk::to_cp437('☺'), fg: RGB::named(rltk::RED), bg: RGB::named(rltk::BLACK) })
+            .with(LeftMover {})
             .build();
     }
 
