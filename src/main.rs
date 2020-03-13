585fe2f8d43cac46f2620c9f162cc0e99fa127e5
Skareeg <skareeg@gmail.com>
2020-03-13 12:34:45 -0400

    Chapter 2 End. Player now moves.

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,32 @@ struct Renderable {
 }
 
 #[derive(Component)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for(_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        }
+    }
+}
+
+#[derive(Component)]
 struct LeftMover {}
 
 struct LeftWalker {}
@@ -50,6 +76,7 @@ impl GameState for State {
         ctx.cls();
         ctx.print(1, 1, "Hello Rust World!");
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -75,11 +102,13 @@ fn main() {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
         .with(Renderable { glyph: rltk::to_cp437('@'), fg: RGB::named(rltk::YELLOW), bg: RGB::named(rltk::BLACK) })
+        .with(Player {})
         .build();
 
     for i in 0..10 {
